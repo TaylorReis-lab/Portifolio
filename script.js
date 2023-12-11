0fcf8b50a82fc09187b102a8e7a791edcbe24d9e
TaylorReis-lab <reist98822@gmail.com>
2023-12-11 17:24:37 -0300

    add all

---
+++ script.js
@@ -0,0 +1,7 @@
+const dowloadCurriculum = document.querySelector(".cv-btn")
+
+function download() {
+    window.open("https://drive.google.com/file/d/1PozgkwJm7DuxI0Z2AVA5Q_P_shqfXWIz/view", "_blak")
+};
+
+dowloadCurriculum.addEventListener("click", download)
