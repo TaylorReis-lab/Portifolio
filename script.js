8e23fe7d9aef50cbfcb5e9f5d8fecb7d36d467e9
TaylorReis-lab <reist98822@gmail.com>
2023-12-12 17:40:34 -0300

    Update css and add new pag, creating functions js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 const dowloadCurriculum = document.querySelector(".cv-btn")
+const buttonAbout = document.querySelector(".button-about")
+const buttonSkills = document.querySelector(".button-skills")
+const list = document.querySelector(".rule")
+let myLi = ""
 
 function download() {
     window.open("https://drive.google.com/file/d/1PozgkwJm7DuxI0Z2AVA5Q_P_shqfXWIz/view", "_blak")
 };
 
+function mostAbout() {
+    list.innerHTML = `
+    <p>Meu nome é Taylor Reis, Atualmente trabalho como freelancer realizando projetos, e sempre me empenhando e sempre aprimorando
+        meus conhecimentos na área, focado em desenvolvimento Full Stack, atualmente trabalho como freelancer, realizando projetos pessoais.
+    </p>
+    `
+}
+
+function mostSkill() {
+    list.innerHTML = `
+    <h1>Hard Skills</h1>
+    <p>
+    </p>
+    `
+}
+
+
+buttonSkills.addEventListener("click", mostSkill)
+buttonAbout.addEventListener("click", mostAbout)
 dowloadCurriculum.addEventListener("click", download)
