ac362254aa72e380fab5f7e39d004365a6470d66
TaylorReis-lab <reist98822@gmail.com>
2023-12-17 23:13:52 -0300

    update all files

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const dowloadCurriculum = document.querySelector(".cv-btn")
 const buttonAbout = document.querySelector(".button-about")
 const buttonSkills = document.querySelector(".button-skills")
-const list = document.querySelector(".rule")
+const buttonPortfo = document.querySelector(".button-portfolio")
+const list = document.querySelector(".social")
 let myLi = ""
 
 function download() {
@@ -18,13 +19,34 @@ function mostAbout() {
 
 function mostSkill() {
     list.innerHTML = `
+    <ul id="ulSkills">
     <h1>Hard Skills</h1>
-    <p>
-    </p>
+        <li>Git</li>
+        <li>Github</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>React</li>
+        <li>JavaScript</li>
+    </ul>
+    `
+}
+
+function mostPort() {
+    dowloadCurriculum.style.display = "none"
+    list.innerHTML = `
+    <ul class="links" >
+        <li><a href="https://taylorreis-lab.github.io/Nikel/"><i class="bi bi-globe2"></i></a>Projeto Nikel Gerenciador de Gastos</li>
+        <br>
+        <li><a href="https://taylorreis-lab.github.io/Projeto-We-Care/"><i class="bi bi-globe2"></i></a>Projeto We Care Pet Shopping</li>
+        <br>
+        <li><a href="https://taylorreis-lab.github.io/Jokenp--Game/"><i class="bi bi-globe2"></i></a>Projeto Jokenpô</li>
+    </ul>
     `
 }
 
 
+
+buttonPortfo.addEventListener("click", mostPort)
 buttonSkills.addEventListener("click", mostSkill)
 buttonAbout.addEventListener("click", mostAbout)
 dowloadCurriculum.addEventListener("click", download)
